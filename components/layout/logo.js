590870e585cf5943b0b7e4f1f3b84cd1a2003531
unknown <saurabhkhoshya@gmail.com>
2021-10-19 15:31:31 +0530

    notification

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,6 +1,6 @@
 import classes from "./logo.module.css";
 function Logo() {
-  return <div className={classes.logo}>Max Next Blog</div>;
+  return <div className={classes.logo}>Saurabh Next Blog</div>;
 }
 
 export default Logo;
